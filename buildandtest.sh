e03f443a6de1ca4c1f693eda17d6f867cfa3b776
24hoursmedia <info@24hoursmedia.com>
2016-08-07 20:37:01 +0200

    renamed image

--- buildandtest.sh
+++ buildandtest.sh
@@ -3,4 +3,5 @@
 # builds the image, and runs a test container named dxstack1_base_test (removes it first if it exists)
 
 docker rm dxstack1_synced_test || echo ''
-docker build .  -t 24hoursmedia/dxstack1:synced && docker run --name=dxstack1_synced_test -ti 24hoursmedia/dxstack1:synced -p 11080:80
+docker build .  -t 24hoursmedia/dxstack1-synced && docker run --name=dxstack1_synced_test -ti 24hoursmedia/dxstack1-synced
+ -p 11080:80
