a17160f6295f5f0f9e024bb169d9c4aa134625e9
24hoursmedia <info@24hoursmedia.com>
2016-08-08 22:58:12 +0200

    docker-dxstack1-synced-21 var cache nginx does not exist

--- docker/nginx/setup.sh
+++ docker/nginx/setup.sh
@@ -9,6 +9,10 @@ mkdir -p $DX_NGINX_VHOSTS_DIR
 mkdir -p $DX_NGINX_EXTRA_VHOSTS_DIR
 cat ./nginx/etc/vservers/default.vserver | ./parse_env.sh > $DX_NGINX_VHOSTS_DIR/default.vserver
 
+mkdir -p /var/cache/nginx
+chown -R nginx /var/cache/nginx
+
+
 # replace nginx file
 cat ./nginx/etc/nginx.conf | ./parse_env.sh > /etc/nginx/nginx.conf
 
