3accba39c52a7c7428695acd431fa99f76011238
24hoursmedia <info@24hoursmedia.com>
2016-08-08 22:28:04 +0200

    docker-dxstack1-synced-2 mysql script tries to recreate user

--- docker/mariadb/setup.sh
+++ docker/mariadb/setup.sh
@@ -11,4 +11,5 @@ chown mysql /var/log/mysql
 
 cat ./mariadb/etc/my.cnf.d/z-4gb.cnf | ./parse_env.sh > /etc/my.cnf.d/z-4gb.cnf
 cat ./mariadb/etc/my.cnf.d/zz-last.cnf | ./parse_env.sh > /etc/my.cnf.d/zz-last.cnf
-cat ./mariadb/create_user.sql | ./parse_env.sh > /tmp/create_user.sql
+
+
--- docker/mariadb/start.sh
+++ docker/mariadb/start.sh
@@ -3,14 +3,20 @@
 # test if /var/lib/mysql is empty..
 if [ "$(ls -A /var/lib/mysql/)" ]; then
      echo "/var/lib/mysql not empty, skip initializing"
+
+     # just start
+     /etc/init.d/mysql start
 else
      echo "/var/lib/mysql empty, initialize"
      mysql_install_db --user=mysql --datadir=/tmp/mysql
      cp -R /tmp/mysql/* /var/lib/mysql/
      chmod -R 777 /var/lib/mysql
      rm -R /tmp/mysql
+
+     /etc/init.d/mysql start
+
+     cat ./mariadb/create_user.sql | ./parse_env.sh > /tmp/create_user.sql
+     mysql -uroot < /tmp/create_user.sql
+     unlink /tmp/create_user.sql;
 fi
 
-/etc/init.d/mysql start
-mysql -uroot < /tmp/create_user.sql
-unlink /tmp/create_user.sql;
