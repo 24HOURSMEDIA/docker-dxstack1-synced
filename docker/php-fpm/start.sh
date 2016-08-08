2e0905ebe75aa9a135bba18029a62d1612ddb625
24hoursmedia <info@24hoursmedia.com>
2016-08-08 22:26:46 +0200

    docker-dxstack1-synced-12 make php-fpm pool configuration possible example php-fpm config placed in share

--- docker/php-fpm/start.sh
+++ docker/php-fpm/start.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env sh
 export DX_PHPFPM_EXTRA_CONF_DIR=$DX_VOLUME_ETC/php-fpm.d
 mkdir -p $DX_PHPFPM_EXTRA_CONF_DIR
+
+# copy example file to extra conf dir if not exist
+if [ -f "$DX_PHPFPM_EXTRA_CONF_DIR/www.conf.example" ]; then
+ echo ""
+else
+    cat ./php-fpm/etc/php-fpm.d/www.conf.example | ./parse_env.sh > $DX_PHPFPM_EXTRA_CONF_DIR/www.conf.example
+fi
+
 php-fpm
