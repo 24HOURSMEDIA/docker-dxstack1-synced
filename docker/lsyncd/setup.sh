fbc58e24a76b62ac12dcb8fc8cb386236204cf7d
24hoursmedia <info@24hoursmedia.com>
2016-08-14 19:26:27 +0200

    docker-dxstack1-synced-36 use dxstack1 v1.5 base image use new base image and remove unused entries

--- docker/lsyncd/setup.sh
+++ docker/lsyncd/setup.sh
@@ -2,12 +2,4 @@
 
 mkdir -p /usr/local/etc/lsyncd
 
-
-# download rsync 3.1 for advanced options
-rpm -Uvh http://mirror.symnds.com/distributions/gf/el/7/plus/x86_64/rsync-3.1.1-6.gf.el7.x86_64.rpm
-
-yum -y install lsyncd
-
-
-
 cat ./lsyncd/etc/dxstack1-synced.conf | ./parse_env.sh > /usr/local/etc/lsyncd/dxstack1-synced.conf
